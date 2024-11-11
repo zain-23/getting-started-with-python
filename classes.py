6b1095f5d697051fbe57b0a741d6a45a991af47d
Syed zain ali shah <zaingit780@gmail.com>
2024-11-11 07:23:52 +0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,23 +1,14 @@
 class Flight():
-    def __init__(self, capacity):
+    def __init__(self,capacity):
         self.capacity = capacity
-        self.passesgers = []
+        self.passengers = []
         
     def add_passengers(self,name):
-        if not self.open_seats():
-         return  False
-        self.passesgers.append(name)
+        if not self.seat_available():
+            return False
+        self.passengers.append(name)
         return True
-    def open_seats(self):
-        return self.capacity - len(self.passesgers)
     
-flight = Flight(3)     
-
-people = ["Harry","Ron","Hermonian","Ginny"] 
- 
-for person in people:
-    success = flight.add_passengers(person)
-    if success:
-        print(f"Added {person} to the flight")
-    else:
-        print(f"Seats not available")
+    def seat_available(self):
+        return self.capacity - len(self.passengers)
+    
